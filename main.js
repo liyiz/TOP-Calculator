8e355e703258dc152cd4d339e732abb61021c0ac
Liyi Z. <5160472+liyiz@users.noreply.github.com>
2024-12-04 13:10:49 -0500

    Add initial js structure

---
+++ main.js
@@ -0,0 +1,35 @@
+// When page loads
+
+// When buttons are pressed
+
+function operate(operator, a, b) {
+    let output = 0;
+    if (operator === 'add') {
+        output = add(a, b);
+    }
+    if (operator === 'subtract') {
+        output = subtract(a, b);
+    }
+    if (operator === 'multiply') {
+        output = multiply(a, b);
+    }
+    if (operator === 'divide') {
+        output = divide(a, b);
+    }
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
